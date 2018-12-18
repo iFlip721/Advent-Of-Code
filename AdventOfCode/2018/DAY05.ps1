44555e76ab84511e372e2ea6a747ac2515fb03ac
iFlip721 <i.Flip721@gmail.com>
2018-12-18 10:23:08 -0500

    DAY 5 Part 1 Complete

---
+++ AdventOfCode/2018/DAY05.ps1
@@ -0,0 +1,20 @@
+﻿# DAY 5
+
+$day5input = Get-Content -Path C:\Temp\AdventOfCode\2018\Input-DAY5.txt
+
+$lastLoop = 0
+$currentLoop = 0
+$char = 65
+
+do {
+    $lastLoop = $currentLoop
+    for($charIncrement = 0;$charIncrement -lt 26;$charIncrement++) {
+        $upperChar = [char]($char + $charIncrement)
+        $lowerChar = [char]($char + 32 + $charIncrement)
+        $day5input = $day5input.Replace("$lowerChar$upperChar",'')
+        $day5input = $day5input.Replace("$upperChar$lowerChar",'')
+    }
+
+    $currentLoop = $day5input.Length
+    $currentLoop
+} while ($lastLoop -ne $currentLoop)
